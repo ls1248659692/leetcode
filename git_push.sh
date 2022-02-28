d361b105ce4c264e5932427b75477702c4b346bb
jam <1248659692@qq.com>
2022-02-28 23:07:14 +0800

    [update](2022-02-28 23:07:13): auto commit and push

---
+++ git_push.sh
@@ -0,0 +1,9 @@
+# !/bin/sh
+
+today=$(date "+%Y-%m-%d %H:%M:%S")
+commit_msg="[update](${today}): auto commit and push"
+echo $commit_msg
+git add -A
+git commit -m "${commit_msg}"
+git push origin master
+echo "push code success"
