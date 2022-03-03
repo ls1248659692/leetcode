543732bfe23ae2c8713f9da0bc11ab83a4f15977
jam <1248659692@qq.com>
2022-03-03 23:14:35 +0800

    [update](2022-03-03 23:14:32): auto commit and push

--- git_push.sh
+++ git_push.sh
@@ -3,6 +3,7 @@
 today=$(date "+%Y-%m-%d %H:%M:%S")
 commit_msg="[update](${today}): auto commit and push"
 echo $commit_msg
+git pull origin master
 git add -A
 git commit -m "${commit_msg}"
 git push origin master
